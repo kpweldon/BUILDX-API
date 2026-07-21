8432cb3dd1204346113421ac88913bb4aa1ae241
Kweldon BuildX <kevinw.buildx@gmail.com>
2026-07-20 20:16:26 -0400

    Delete descript_api directory

--- descript_api/__init__.py
+++
@@ -1,9 +0,0 @@
-from .client import DescriptClient
-from .exceptions import DescriptAPIError, DescriptAuthError, DescriptRateLimitError
-
-__all__ = [
-    "DescriptClient",
-    "DescriptAPIError",
-    "DescriptAuthError",
-    "DescriptRateLimitError",
-]
